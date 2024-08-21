752fa2a81ee86dacdb642228e2a874c54807af32
Tiien Dat <tiiendat27@gmail.com>
2024-08-21 20:41:17 +0700

    v0.1.5

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,10 +4,13 @@ import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './passport/local-auth.guard';
 import { Public } from '@/decorator/customize';
 import { RegisterDto } from './dto/create-auth.dto';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService,
+    private readonly mailerService: MailerService
+  ) {}
 
   
   @Post("login")
@@ -20,5 +23,19 @@ export class AuthController {
   @Public()
   register(@Body() registerDto:RegisterDto) {
     return this.authService.handleRegister(registerDto);
+  }
+
+  @Get('mail')
+  @Public()
+  sendMail() {
+    this.mailerService
+      .sendMail({
+        to: 'nguyentiendat120299@gmail.com', 
+        from: 'noreply@nestjs.com', // sender address
+        subject: 'Testing Nest MailerModule ✔', // Subject line
+        text: 'welcome', // plaintext body
+        html: '<b>welcome</b>', // HTML body content
+      })
+    return 'ok'
   }
 }
